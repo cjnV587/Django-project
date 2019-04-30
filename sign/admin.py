ad9f5d3bf69b92dda229ecae5d2e2c2ab6d25f96
cjnV587 <383351751@qq.com>
2019-04-30 17:26:08 +0800

    admin后台管理；

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from sign.models import Guest, Event
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time', ]
+    search_fields = ['name']  #搜索栏
+    list_filter = ['status']  #过滤器
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'sign', 'create_time', 'event']
+    search_fields = ['realname', "phone"]  #搜索栏
+    list_filter = ['sign']                 #过滤器
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Guest, GuestAdmin)
